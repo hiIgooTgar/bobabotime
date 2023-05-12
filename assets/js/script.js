ae5c3e283d42682eb004d01144555bd17c6fbeb7
Igo RPL <igorpl1289845@gmail.com>
2023-05-12 08:59:39 +0700

    Membuat Website Sederhana Project-1

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,22 @@ window.addEventListener("scroll", function () {
   var navSticky = document.querySelector(".navbar");
   navSticky.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// scroll delay reveal javascript
+
+const sr = ScrollReveal({
+  distance: "30px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(" .navbar", {delay: 140, origin: "top"});
+sr.reveal(" .social-media", {delay: 140, origin: "bottom"});
+sr.reveal(" .home-content", {delay: 190, origin: "left"});
+sr.reveal(" .heading", {delay: 120, origin: "left"});
+sr.reveal(" .about-content", {delay: 120, origin: "top"});
+sr.reveal(" .menu-box", {delay: 100, origin: "top"});
+sr.reveal(" .title-menu", {delay: 120, origin: "top"});
+sr.reveal(" .dessert-box", {delay: 100, origin: "top"});
+sr.reveal(" .title-dessert", {delay: 120, origin: "top"});
+sr.reveal(" .contact", {delay: 120, origin: "top"});
